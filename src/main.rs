6dc56e0c860973629a081b32769c7322f8ac6b16
Alex Zasorin <zasorin.alek@gmail.com>
2025-10-05 14:36:06 -0700

    chore: remove unnecessary comments

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,8 @@ struct State {
 
 register_plugin!(State);
 
-// NOTE: you can start a development environment inside Zellij by running `zellij -l zellij.kdl` in
-// this plugin's folder
-//
-// More info on plugins: https://zellij.dev/documentation/plugins
-
 impl ZellijPlugin for State {
     fn load(&mut self, _configuration: BTreeMap<String, String>) {
-        // runs once on plugin load, provides the configuration with which this plugin was loaded
-        // (if any)
-        //
-        // this is a good place to `subscribe` (https://docs.rs/zellij-tile/latest/zellij_tile/shim/fn.subscribe.html)
-        // to `Event`s (https://docs.rs/zellij-tile/latest/zellij_tile/prelude/enum.Event.html)
-        // and `request_permissions` (https://docs.rs/zellij-tile/latest/zellij_tile/shim/fn.request_permission.html)
-
         request_permission(&[
             PermissionType::Reconfigure,
             PermissionType::ReadApplicationState,
@@ -43,9 +31,6 @@ impl ZellijPlugin for State {
     }
 
     fn update(&mut self, event: Event) -> bool {
-        // react to `Event`s that have been subscribed to (and the plugin has permissions for)
-        // return true if this plugin's `render` function should be called for the plugin to render
-        // itself
         let mut should_render = false;
 
         // TODO: Handle situation where user closes the plugin pane while in awaiting input or waiting for new tab state
@@ -109,11 +94,6 @@ impl ZellijPlugin for State {
 
     fn pipe(&mut self, pipe_message: PipeMessage) -> bool {
         let mut should_render = false;
-        // react to data piped to this plugin from the CLI, a keybinding or another plugin
-        // read more about pipes: https://zellij.dev/documentation/plugin-pipes
-        // return true if this plugin's `render` function should be called for the plugin to render
-        // itself
-
         if pipe_message.name == "new_tab" {
             if self.awaiting_input {
                 self.awaiting_input = false;
