c72b7a76c5fa5c629a70879f11ad61c7e472fdee
Alex Zasorin <zasorin.alek@gmail.com>
2025-10-30 23:17:47 -0700

    wip: attempt to get plugin working with keybinding in config

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use std::collections::BTreeMap;
 #[derive(Debug, Default)]
 struct State {
     new_tab_name: String,
-    awaiting_input: bool,
     permissions_granted: bool,
+    tab_infos: Vec<TabInfo>,
+    pane_manifest: PaneManifest,
+    focused_tab: Option<TabInfo>,
 }
 
 register_plugin!(State);
@@ -19,37 +21,37 @@ impl ZellijPlugin for State {
             PermissionType::ChangeApplicationState,
         ]);
         subscribe(&[
-            EventType::ModeUpdate,
-            EventType::TabUpdate,
             EventType::Key,
             EventType::PermissionRequestResult,
+            EventType::TabUpdate,
+            EventType::PaneUpdate,
         ]);
-
-        if self.permissions_granted {
-            hide_self();
-        }
     }
 
     fn update(&mut self, event: Event) -> bool {
         let mut should_render = false;
 
-        // TODO: Handle situation where user closes the plugin pane while in awaiting input or waiting for new tab state
         match event {
-            Event::ModeUpdate(_) => {
-                let plugin_ids = get_plugin_ids();
-
-                // FIXME: Avoid rebinding if already bound? Other scenarios where this should be
-                // bound?
-                bind_key(plugin_ids.plugin_id);
-            }
             Event::PermissionRequestResult(permission) => {
                 self.permissions_granted = match permission {
                     PermissionStatus::Granted => true,
                     PermissionStatus::Denied => false,
                 };
 
-                if self.permissions_granted {
-                    hide_self();
+                // if self.permissions_granted {
+                //     setup_plugin_pane();
+                // }
+            }
+            Event::PaneUpdate(pane_manifest) => {
+                // setup_plugin_pane();
+                self.pane_manifest = pane_manifest;
+            }
+            Event::TabUpdate(tab_infos) => {
+                self.tab_infos = tab_infos.clone();
+                let focused_tab = get_focused_tab(&tab_infos);
+
+                if let Some(tab) = focused_tab {
+                    self.focused_tab = Some(tab);
                 }
             }
             Event::Key(key) => match key.bare_key {
@@ -58,25 +60,20 @@ impl ZellijPlugin for State {
                     should_render = true;
                 }
                 BareKey::Enter => {
-                    if self.awaiting_input && !self.new_tab_name.trim().is_empty() {
+                    if !self.new_tab_name.trim().is_empty() {
                         new_tab(Some(&self.new_tab_name), None);
 
-                        self.awaiting_input = false;
                         self.new_tab_name = String::new();
                         should_render = true;
                         hide_self();
                     }
                 }
                 BareKey::Esc => {
-                    if self.awaiting_input {
-                        self.awaiting_input = false;
-                        self.new_tab_name = String::new();
-                        should_render = true;
-                        hide_self();
-                    }
+                    self.new_tab_name = String::new();
+                    hide_self();
                 }
                 BareKey::Backspace => {
-                    if self.awaiting_input && !self.new_tab_name.is_empty() {
+                    if !self.new_tab_name.is_empty() {
                         self.new_tab_name.truncate(self.new_tab_name.len() - 1);
                         should_render = true;
                     }
@@ -92,70 +89,60 @@ impl ZellijPlugin for State {
         should_render
     }
 
-    fn pipe(&mut self, pipe_message: PipeMessage) -> bool {
-        let mut should_render = false;
-        if pipe_message.name == "new_tab" {
-            if self.awaiting_input {
-                self.awaiting_input = false;
-                self.new_tab_name = String::new();
-                hide_self();
-            } else {
-                self.awaiting_input = true;
-
-                show_self(true);
-
-                let plugin_id = get_plugin_ids().plugin_id;
-
-                let coordinates = FloatingPaneCoordinates::new(
-                    Some(String::from("40%")),
-                    Some(String::from("6")),
-                    Some(String::from("20%")),
-                    Some(String::from("3")),
-                    Some(false),
-                );
-
-                match coordinates {
-                    None => eprintln!("Invalid floating pane coordinates."),
-                    Some(floating_coordinates) => {
-                        change_floating_panes_coordinates(vec![(
-                            PaneId::Plugin(plugin_id),
-                            floating_coordinates,
-                        )]);
-                    }
-                }
-
-                rename_pane_with_id(
-                    PaneId::Plugin(plugin_id),
-                    String::from("Enter new tab name:"),
-                );
-
-                should_render = true;
-            }
-        }
-
-        should_render
+    fn render(&mut self, _rows: usize, _cols: usize) {
+        print_text_with_coordinates(Text::new(self.new_tab_name.to_string()), 0, 1, None, None);
     }
 
-    fn render(&mut self, _: usize, _: usize) {
-        // FIXME: Center this somehow?
-        print!("{}", self.new_tab_name);
+    fn pipe(&mut self, pipe_message: PipeMessage) -> bool {
+        if pipe_message.name == "new_tab" {
+            let plugin_id = get_plugin_ids().plugin_id;
+
+            let mut plugin_pane_present = false;
+            self.pane_manifest.panes.iter().for_each(|(pane_id, _)| {
+                if (*pane_id) as u32 == plugin_id {
+                    plugin_pane_present = true;
+                }
+            });
+
+            if let Some(tab) = &self.focused_tab {
+                if !plugin_pane_present {
+                    break_panes_to_tab_with_index(&[PaneId::Plugin(plugin_id)], tab.position, true);
+                }
+            }
+
+            toggle_pane_embed_or_eject();
+            setup_plugin_pane();
+
+            show_self(true);
+        }
+
+        true
     }
 }
 
-pub fn bind_key(own_plugin_id: u32) {
-    let new_config = format!(
-        "
-        keybinds {{
-            shared {{
-                bind \"Ctrl n\" {{
-                    MessagePluginId {} {{
-                        name \"new_tab\"
-                    }}
-                }}
-            }}
-        }}
-        ",
-        own_plugin_id
+fn setup_plugin_pane() {
+    let plugin_id = get_plugin_ids().plugin_id;
+
+    let coordinates = FloatingPaneCoordinates::new(
+        Some(String::from("40%")),
+        Some(String::from("6")),
+        Some(String::from("20%")),
+        Some(String::from("3")),
+        Some(false),
     );
-    reconfigure(new_config, false);
+
+    match coordinates {
+        None => eprintln!("Invalid floating pane coordinates."),
+        Some(floating_coordinates) => {
+            change_floating_panes_coordinates(vec![(
+                PaneId::Plugin(plugin_id),
+                floating_coordinates,
+            )]);
+        }
+    }
+
+    rename_pane_with_id(
+        PaneId::Plugin(plugin_id),
+        String::from("Enter new tab name:"),
+    );
 }
